efabe1866e8f5da9cbd6c8f7bcfc033e77e7b697
Janisku7 <janisku7@gmail.com>
2026-05-23 13:07:45 +0300

    update great

--- JT7SKU_Aspire_DuneAwakens_SelfHosted.AppHost/AppHost.cs
+++ JT7SKU_Aspire_DuneAwakens_SelfHosted.AppHost/AppHost.cs
@@ -6,5 +6,8 @@ var rakentaja = DistributedApplication.CreateBuilder(args);
 // Port Forwarding portit 7777-7810 UDP for the game servers. Ja 31982 TCP For RMQ 
 var rabbitMq = rakentaja.AddRabbitMQ("viestinta")
     .WithManagementPlugin(port:31982);
+var pg = rakentaja.AddPostgres("pg");
+var pgDB = pg.AddDatabase("pgdb");
+
 
 rakentaja.Build().Run();
