eca913d8e3da0668f4ce8d52f70bc1dee11c7418
Janisku7 <janisku7@gmail.com>
2026-05-23 11:36:06 +0300

    dyynit

--- JT7SKU_Aspire_DuneAwakens_SelfHosted.AppHost/AppHost.cs
+++ JT7SKU_Aspire_DuneAwakens_SelfHosted.AppHost/AppHost.cs
@@ -1,3 +1,10 @@
-var builder = DistributedApplication.CreateBuilder(args);
+var rakentaja = DistributedApplication.CreateBuilder(args);
 
-builder.Build().Run();
+// Ↄ⋃⋂Ⅽ awakening Dyyni herääminen
+
+// Keycloack jotta token hengis, postgress viel mahollisuus battlegroupissa
+// Port Forwarding portit 7777-7810 UDP for the game servers. Ja 31982 TCP For RMQ 
+var rabbitMq = rakentaja.AddRabbitMQ("viestinta")
+    .WithManagementPlugin(port:31982);
+
+rakentaja.Build().Run();
