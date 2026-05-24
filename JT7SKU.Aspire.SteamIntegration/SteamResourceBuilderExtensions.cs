c48f98846c1f60917176830296ab420b6c2710a0
Janisku7 <janisku7@gmail.com>
2026-05-24 13:48:58 +0300

    pientä säätöä

--- JT7SKU.Aspire.SteamIntegration/SteamResourceBuilderExtensions.cs
+++ JT7SKU.Aspire.SteamIntegration/SteamResourceBuilderExtensions.cs
@@ -8,12 +8,17 @@ namespace JT7SKU.Aspire.Integrations.Steam
 {
     public static class SteamResourceBuilderExtensions
     {
+        private const string KayttajaYmpVarNimi = "STEAM_SISAANTULEVA_KAYTTAJA";
+        private const string SalasanaYmpVarNimi = "STEAM_SISAANTULEVA_SALURI";
         public static IResourceBuilder<SteamResource> AddSteamDB(
-            this IDistributedApplicationBuilder rakentaja, [ResourceName] string nimi, int? port = null)
+            this IDistributedApplicationBuilder rakentaja, [ResourceName] string nimi, int? port = null,
+            IResourceBuilder<ParameterResource>? kayttajanimi = null, IResourceBuilder<ParameterResource>? salasana = null)
         {
-            var resourssi = new SteamReasource(nimi);
+            var salasanaParametri = salasana?.Resource ?? ParameterResourceBuilderExtensions.CreateDefaultPasswordParameter(rakentaja, $"{nimi}-salasana");
+            var resourssi = new SteamResource(nimi,kayttajanimi?.Resource,salasanaParametri);
 
             return rakentaja.AddResource(resourssi);
+                
         }
     }
 }
